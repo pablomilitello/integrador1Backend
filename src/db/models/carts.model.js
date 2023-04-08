8ecedad4834bb16745c55cbe542416dfbb9e7f86
“Pablo <pmilitelloaveni@gmail.com>
2023-04-08 20:55:48 -0300

    agrego funciones al CartManagerMongo

--- src/db/models/carts.model.js
+++ src/db/models/carts.model.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
-const cartsSchema = new mongoose.Schema({});
+const cartsSchema = new mongoose.Schema({
+  products: {
+    type: Array,
+  },
+});
 
 export const cartModel = mongoose.model('Carts', cartsSchema);
